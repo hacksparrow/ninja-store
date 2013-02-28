6f38b87d8492675d11bfb3275dd2e0e3ca79585c
Hack Sparrow <captain@hacksparrow.com>
2013-03-01 01:44:43 +0530

    Update store.js

--- routes/store.js
+++ routes/store.js
@@ -1,21 +1,3 @@
-// handler for homepage
-exports.home = function(req, res) {
-	// if user is not logged in, ask them to login
-	if (typeof req.session.username == 'undefined') res.render('home', { title: 'Ninja Store'});
-	// if user is logged in already, take them straight to the items list
-	else res.redirect('/items');
-};
-
-// handler for form submitted from homepage
-exports.home_post_handler = function(req, res) {
-	// if the username is not submitted, give it a default of "Anonymous"
-	username = req.body.username || 'Anonymous';
-	// store the username as a session variable
-	req.session.username = username;
-	// redirect the user to homepage
-	res.redirect('/');
-};
-
 // our 'database'
 var items = {
     SKN:{name:'Shuriken', price:100},
@@ -23,6 +5,24 @@ var items = {
     CGI:{name:'Chigiriki', price:250},
     NGT:{name:'Naginata', price:900},
     KTN:{name:'Katana', price:1000}
+};
+
+// handler for homepage
+exports.home = function(req, res) {
+    // if user is not logged in, ask them to login
+    if (typeof req.session.username == 'undefined') res.render('home', { title: 'Ninja Store'});
+    // if user is logged in already, take them straight to the items list
+    else res.redirect('/items');
+};
+
+// handler for form submitted from homepage
+exports.home_post_handler = function(req, res) {
+    // if the username is not submitted, give it a default of "Anonymous"
+    username = req.body.username || 'Anonymous';
+    // store the username as a session variable
+    req.session.username = username;
+    // redirect the user to homepage
+    res.redirect('/');
 };
 
 // handler for displaying the items
