19f118296d9237ff920d207359a34f8c726490fc
Jan Greth <github@greth.me>
2016-03-02 16:47:00 +0100

    v0.0.2 Now compatible with Express 4.x

--- routes/store.js
+++ routes/store.js
@@ -1,4 +1,4 @@
-// our 'database'
+﻿// our 'database'
 var items = {
     SKN:{name:'Shuriken', price:100},
     ASK:{name:'Ashiko', price:690},
@@ -18,7 +18,7 @@ exports.home = function(req, res) {
 // handler for form submitted from homepage
 exports.home_post_handler = function(req, res) {
     // if the username is not submitted, give it a default of "Anonymous"
-    username = req.body.username || 'Anonymous';
+    var username = req.body.username || 'Anonymous';
     // store the username as a session variable
     req.session.username = username;
     // redirect the user to homepage
